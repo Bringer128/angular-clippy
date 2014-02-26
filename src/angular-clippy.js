67fdda817d66f89d64260202e81e1444469f4749
Hatem Nassrat <hnassrat@gmail.com>
2014-02-26 13:23:37 +0000

    Adding configuration parameters.

--- src/angular-clippy.js
+++ src/angular-clippy.js
@@ -1,18 +1,34 @@
-angular
-  .module('angularClippy', [])
-  .config(function() {
-    clippy.BASE_PATH = './Agents/';
-  })
-  .directive('clippy', [function() {
-    return {
-      restrict: 'E',
-      transclude: false,
-      scope: true,
-      link: function(scope, element, attrs) {
-        clippy.load('Clippy', function(agent) {
-	  agent.moveTo(element.offset().left, element.offset().top);
-	  agent.show();
-	});
-      }
-    };
-  }]);
+/*global angular, clippy */
+(function(){
+  'use strict';
+  var scripts = document.getElementsByTagName('script')
+    , currentScriptPath = scripts[scripts.length-1].src
+    ;
+
+  angular
+    .module('angularClippy', [])
+    .config(function() {
+      clippy.BASE_PATH = currentScriptPath.replace('angular-clippy.js', 'Agents/');
+    })
+    .directive('clippy', [function() {
+      return {
+        restrict: 'E',
+        transclude: false,
+        scope: true,
+        link: function(scope, element, attrs) {
+          var agentName = attrs.agent || 'Clippy'
+            , callback = attrs.setup
+            ;
+          clippy.load(agentName, function(agent) {
+            agent.moveTo(element.offset().left, element.offset().top);
+            agent.show();
+            if (callback) {
+              scope.$apply(function(scope){
+                scope[callback](agent);
+              });
+            }
+          });
+        }
+      };
+    }]);
+}());
